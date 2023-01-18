c4e38b4c461644fb5357e9be728a2e35778fd5cf
maugois <gois.mauricio12@gmail.com>
2023-01-18 17:19:47 -0300

    preparação do index

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php ?>
 <!DOCTYPE html>
-<html lang="pt_BR">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,7 +9,28 @@
     <link rel="stylesheet" href="css/estilo.css">
     <title>Chuleta Quente Churrascaria</title>
 </head>
-<body>
-    <h3>Index</h3>     
+<body class="fundofixo">
+    <!-- area de menu -->
+    <?php include 'menu_publico.php'; ?>
+    <a name="home">&nbsp;</a>
+
+    <main class="container">
+        <!-- area de carousel -->
+        <?php include 'carousel.php'; ?>
+
+        <!-- area de destaque -->
+        <a name="destaques">&nbsp;</a>
+        <?php include 'produtos_destaque.php'; ?>
+
+        <!-- area geral de produtos -->
+        <a name="produtos ">&nbsp;</a>
+        <?php include 'produtos_geral.php'; ?>
+
+        <!-- rodapé -->
+        <footer class="panel-footer" style="background: none;">
+            <?php include 'rodape.php'; ?>
+            <a name="contato"></a>
+        </footer>
+    </main>
 </body>
 </html>
