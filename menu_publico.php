e0ed725c8711b7e02e7814f6d3f44343dec06ca2
maugois <gois.mauricio12@gmail.com>
2023-01-20 17:17:08 -0300

    rodape e página de tipos

--- menu_publico.php
+++ menu_publico.php
@@ -24,7 +24,7 @@ $rows_tipos = $lista_tipos->fetch_all();
                     <span class="icon-bar"></span>
                 </button>
                 <a href="index.php" class="navbar-brand">
-                    <img src="" alt="Logotipo Churrascaria do Mau">
+                    <img src="192x32" alt="Logotipo Churrascaria do Mau">
                 </a>
             </div>
             <!-- fecha agrupamento mobile -->
