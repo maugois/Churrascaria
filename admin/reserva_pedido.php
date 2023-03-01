1a7552794880795dc83e7959bfb3327e0627ab07
maugois <gois.mauricio12@gmail.com>
2023-03-01 17:26:58 -0300

    tentativa do modal pedido pg

--- admin/reserva_pedido.php
+++ admin/reserva_pedido.php
@@ -1,9 +1,26 @@
 <?php 
 include 'acesso_com.php';
 include '../connection/connect.php';
+// Lista do pedidos
 $lista = $conn->query("select * from tbpedidoreservas where status_pedido = 'Enviado' order by id_pedido_reservas desc");
 $row = $lista->fetch_assoc();
 $rows = $lista->num_rows;
+
+// Informações
+$listaCli = $conn->query("select * from tbclientes");
+$rowCli = $listaCli->fetch_assoc();
+$rowsCli = $listaCli->num_rows;
+
+// Confirmar
+//$Conf = $conn->query("");
+//$rowConf = $Conf->fetch_assoc();
+//$rowsConf = $Conf->num_rows;
+
+// Recusar
+//$lista = $conn->query("s desc");
+//$row = $lista->fetch_assoc();
+//$rows = $lista->num_rows;
+
 ?>
 
 <!DOCTYPE html>
@@ -58,6 +75,11 @@ $rows = $lista->num_rows;
                         </td>
 
                         <td>
+                            <button data-nome="<?php echo $row['id_pedido_reservas']?>" data-id="<?php echo $rowCli['id_cliente']?>" class="informar btn btn-xs btn-block btn-primary">
+                                <span class="glyphicon glyphicon-trash"></span>
+                                <span class="hidden-xs">INFORMAÇÕES</span>
+                            </button>
+
                             <button data-nome="<?php echo $row['id_pedido_reservas']?>" data-id="<?php echo $row['id_pedido_reservas']?>" class="confirmar btn btn-success btn-block btn-xs"> 
                                 <span class="glyphicon glyphicon-refresh"></span>
                                 <span class="hidden-xs">CONFIRMAR</span>
@@ -74,95 +96,129 @@ $rows = $lista->num_rows;
         </table>
     </main>
 
-    <!-- inicio do modal para confirmar... -->
-    <div class="modal fade" id="modalEdit" role="dialog">
+    <!-- inicio do modal para informar... -->
+    <div class="modal fade" id="modalInfo" role="dialog">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
+                    Informações do pedido
                     <button class="close" data-dismiss="modal" type="button">
                         &times;
                     </button>
                 </div>
                 <div class="modal-body">
-                    Deseja mesmo excluir a mesa de Nº?
-                    <form action="tipo_atualizar.php" method="post" name="form_tipo_atualizar" enctype="multipart/form-data" id="form_tipo_atualizar">
-                        <input type="hidden" name="id_tipo" id="id_tipo" value="<?php echo $row['id_tipo']; ?>">
-                        
-                        <label for="rotulo_tipo">Rótulo:</label>
+                
+                    <form action="" method="post" name="" enctype="multipart/form-data" id="">
+                        <label for="nome">Nome:</label>
                         <div class="input-group">
                             <span class="input-group-addon">
-                                <span class="glyphicon glyphicon-tasks" aria-hidden="true"></span>
+                                <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
                             </span>
-                            <input type="text" name="rotulo_tipo" id="rotulo_tipo" class="form-control" placeholder="Digite o rótulo do Tipo" maxlength="100" value="<?php echo $row['rotulo_tipo'] ?>">
+                            <input disabled type="text" name="nome" id="nome" class="form-control" value="<?php echo $rowCli['nome'] ?>">
                         </div>
 
-                        <label for="sigla_tipo">Sigla:</label>
+                        <label for="cpf">CPF:</label>
                         <div class="input-group">
                             <span class="input-group-addon">
-                                <span class="glyphicon glyphicon-cutlery" aria-hidden="true"></span>
+                                <span class="glyphicon glyphicon-book" aria-hidden="true"></span>
                             </span>
                             
-                            <input type="text" name="sigla_tipo" id="sigla_tipo" class="form-control" placeholder="Digite a sigla do Tipo" maxlength="3" value="<?php echo $row['sigla_tipo'] ?>">
+                            <input disabled type="text" name="cpf" id="cpf" class="form-control" value="<?php echo $rowCli['email'] ?>">
+                        </div>
+
+                        <label for="email">E-MAIL:</label>
+                        <div class="input-group">
+                            <span class="input-group-addon">
+                                <span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>
+                            </span>
+                            
+                            <input disabled type="text" name="email" id="email" class="form-control" value="<?php echo $rowCli['cpf'] ?>">
+                        </div>
+
+                        <label for="cod">CÓDIGO:</label>
+                        <div class="input-group">
+                            <span class="input-group-addon">
+                                <span class="glyphicon glyphicon-barcode" aria-hidden="true"></span>
+                            </span>
+                            
+                            <input disabled type="text" name="cod" id="cod" class="form-control" value="<?php echo $row['hash_code'] ?>">
                         </div>
 
                         <hr>
-                        <input type="submit" name="atualizar" id="atualizar" class="btn btn-danger btn-block" value="Atualizar">
+
+                        <button class="btn btn-danger" data-dismiss="modal">
+                            Fechar
+                        </button>
                     </form>
                 </div>
-                <div class="modal-footer">
-                    <a href="#" type="button" class="btn btn-success delete-yes">
-                        Confirmar
-                    </a>
-                    <button class="btn btn-danger" data-dismiss="modal">
-                        Cancelar
+            </div>
+        </div>
+    </div>
+
+    <!-- inicio do modal para confirmar... -->
+    <div class="modal fade" id="modalCon" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <span>Confirmar</span>
+                    <button class="close" data-dismiss="modal" type="button">
+                        &times;
                     </button>
+                </div>
+                <div class="modal-body">
+                    
+                    <form action="" method="post" name="" enctype="multipart/form-data" id="">
+                        
+                        <label for="mesa">Mesa dísponivel:</label>
+                        <div class="input-group">
+                            <span class="input-group-addon">
+                                <span class="glyphicon glyphicon-tags" aria-hidden="true"></span>
+                            </span>
+                            <input type="text" name="mesa" id="mesa" class="form-control" placeholder="Informe a mesa" maxlength="100">
+                        </div>
+
+                        <hr>
+
+                        <input type="submit" name="confirmar" id="confirmar" class="btn btn-success" value="Confirmar">
+                        
+                        <button class="btn btn-danger" data-dismiss="modal">
+                            Cancelar
+                        </button>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
 
     <!-- inicio do modal para recusar... -->
-    <div class="modal fade" id="modalEdit" role="dialog">
+    <div class="modal fade" id="modalRecu" role="dialog">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
+                    <span>RECUSAR</span>
                     <button class="close" data-dismiss="modal" type="button">
                         &times;
                     </button>
                 </div>
                 <div class="modal-body">
-                    Deseja mesmo excluir a mesa de Nº?
-                    <form action="tipo_atualizar.php" method="post" name="form_tipo_atualizar" enctype="multipart/form-data" id="form_tipo_atualizar">
-                        <input type="hidden" name="id_tipo" id="id_tipo" value="<?php echo $row['id_tipo']; ?>">
-                        
-                        <label for="rotulo_tipo">Rótulo:</label>
-                        <div class="input-group">
-                            <span class="input-group-addon">
-                                <span class="glyphicon glyphicon-tasks" aria-hidden="true"></span>
-                            </span>
-                            <input type="text" name="rotulo_tipo" id="rotulo_tipo" class="form-control" placeholder="Digite o rótulo do Tipo" maxlength="100" value="<?php echo $row['rotulo_tipo'] ?>">
-                        </div>
+                    <form action="" method="post" name="" enctype="multipart/form-data" id="">         
 
-                        <label for="sigla_tipo">Sigla:</label>
+                        <label for="mensagem">Mensagem para o cliente:</label>
                         <div class="input-group">
                             <span class="input-group-addon">
-                                <span class="glyphicon glyphicon-cutlery" aria-hidden="true"></span>
+                                <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
                             </span>
-                            
-                            <input type="text" name="sigla_tipo" id="sigla_tipo" class="form-control" placeholder="Digite a sigla do Tipo" maxlength="3" value="<?php echo $row['sigla_tipo'] ?>">
+                            <textarea name="mensagem" max-cols="30" rows="5" placeholder="digite sua mensagem" aria-describedby="basic-addon3" class="form-control" required></textarea>
                         </div>
 
                         <hr>
-                        <input type="submit" name="atualizar" id="atualizar" class="btn btn-danger btn-block" value="Atualizar">
+
+                        <input type="submit" name="enviar" id="enviar" class="btn btn-success" value="Enviar">
+                        
+                        <button class="btn btn-danger" data-dismiss="modal">
+                            Cancelar
+                        </button>
                     </form>
-                </div>
-                <div class="modal-footer">
-                    <a href="#" type="button" class="btn btn-success delete-yes">
-                        Confirmar
-                    </a>
-                    <button class="btn btn-danger" data-dismiss="modal">
-                        Cancelar
-                    </button>
                 </div>
             </div>
         </div>
@@ -171,22 +227,25 @@ $rows = $lista->num_rows;
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="../js/bootstrap.min.js"></script>
 <script type="text/javascript">
+    $('.informar').on('click',function(){
+        var nome = $(this).data('nome'); //busca o nome com a descrição (data-nome)
+        var idInfo = $(this).data('id'); // busca o id (data-id)
+        $('span.nome').text(nome); // insere o nome do item na confirmação
+        $('#modalInfo').modal('show'); // chamar o modal
+    });
+
     $('.confirmar').on('click',function(){
         var nome = $(this).data('nome'); //busca o nome com a descrição (data-nome)
-        var id = $(this).data('id'); // busca o id (data-id)
-        //console.log(id + ' - ' + nome); //exibe no console
+        var idCon = $(this).data('id'); // busca o id (data-id)
         $('span.nome').text(nome); // insere o nome do item na confirmação
-        $('a.delete-yes').attr('href','mesas_excluir.php?id_mesa='+id); //chama o arquivo php para excluir o produto
-        $('#modalEdit').modal('show'); // chamar o modal
+        $('#modalCon').modal('show'); // chamar o modal
     });
 
     $('.recusar').on('click',function(){
         var nome = $(this).data('nome'); //busca o nome com a descrição (data-nome)
-        var id = $(this).data('id'); // busca o id (data-id)
-        //console.log(id + ' - ' + nome); //exibe no console
+        var idRecu = $(this).data('id'); // busca o id (data-id)
         $('span.nome').text(nome); // insere o nome do item na confirmação
-        $('a.delete-yes').attr('href','mesas_excluir.php?id_mesa='+id); //chama o arquivo php para excluir o produto
-        $('#modalEdit').modal('show'); // chamar o modal
+        $('#modalRecu').modal('show'); // chamar o modal
     });
 </script>
 </html>
